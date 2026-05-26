b110939f905161a5926844617f475cb06edf0d86
Владислав <vladislav@MacBook-Neo-Vladislav.local>
2026-05-26 20:57:55 +0400

    fixredis

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -29,4 +29,4 @@ run_migrations
 init_permissions
 #init_redis   # 👈 ДОБАВИТЬ ЭТУ СТРОКУ
 
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --workers 4
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --workers 2
