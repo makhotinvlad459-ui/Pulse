2b50590dce1890729a3bc01de8efa989b2e8311c
Владислав <vladislav@MacBook-Neo-Vladislav.local>
2026-06-11 00:41:35 +0400

    loading

--- backend/app/database.py
+++ backend/app/database.py
@@ -22,9 +22,9 @@ async def get_db() -> AsyncSession:
 
 #async def init_db():
     """Создаёт все таблицы, если их нет."""
-    engine = get_engine()
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    #engine = get_engine()
+    #async with engine.begin() as conn:
+     #   await conn.run_sync(Base.metadata.create_all)
 
 async def init_permissions():
     """Заполняет таблицу permissions начальными правами."""
